dd52f3cfd31877064e4a92e68443264187c3813c
Leondro Lio <github@leondrolio.com>
2020-08-29 16:43:00 +0100

    Import `graphql` into page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,17 @@
+import { graphql } from 'gatsby';
 import React from "react";
+import { Experiences } from "../components/Experiences";
 import Layout from "../components/Layout";
 import PaddingContainer from "../components/PaddingContainer";
-import styles from './index.module.scss';
 import { Projects } from "../components/Projects";
-import { Experiences } from "../components/Experiences";
 import { WelcomeWheel } from '../components/WelcomeWheel';
-import SocialMediaHandles from "../components/SocialMediaHandles";
 
 const IndexPage = ({ data }) => (
   <Layout>
     <WelcomeWheel />
     <PaddingContainer>
-      
       <h1>My Experiences</h1>
       <Experiences />
-
       <h1>My Projects</h1>
       <Projects />
     </PaddingContainer>
