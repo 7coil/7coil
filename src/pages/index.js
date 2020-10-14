5a00775ac9c84afec5e07981b0c80bf1a49a7d35
Leondro Lio <github@leondrolio.com>
2020-10-14 16:27:53 +0100

    Remove experiences section (why?!)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import { graphql } from 'gatsby'
 import React from 'react'
-import { Experiences } from '../components/Experiences'
 import Layout from '../components/Layout'
 import PaddingContainer from '../components/PaddingContainer'
 import { Projects } from '../components/Projects'
@@ -10,8 +9,6 @@ const IndexPage = ({ data }) => (
   <Layout noHeader={true}>
     <WelcomeWheel />
     <PaddingContainer>
-      <h1>My Experiences</h1>
-      <Experiences />
       <h1>My Projects</h1>
       <Projects />
     </PaddingContainer>
